82b115db7aa88e2a673ffa75bedc1b5464e924ff
Jasper Wu <jasper.wu@cargosmart.com>
2020-08-10 15:22:56 +0800

    case: long expression with `+` and `-` and `*` and `minus number` -- refactor2

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -10,34 +10,46 @@ import java.util.stream.Collectors;
 import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Stream.of;
+import static org.apache.commons.lang3.ArrayUtils.remove;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * @author Jasper Wu
  * @date 2020/8/9
  **/
 public class Calculator {
+    public static final String SPLIT_FOR_NUMBER = "\\s[+\\-*]\\s";
+    public static final String SPLIT_FOR_OPERATION = "\\s?-?\\d+\\s?";
+    private final Map<String, Integer> priorityMap;
+
+    {
+        priorityMap = new HashMap<>(8);
+        priorityMap.put("+", 0);
+        priorityMap.put("-", 0);
+        priorityMap.put("*", 1);
+    }
+
     public int calculate(String expression) {
-        String[] nums = expression.split("\\s[+\\-*]\\s");
-        String[] ops = expression.split("\\s?-?\\d+\\s?");
-        if (StringUtils.isBlank(ops[0])) {
-            ArrayUtils.remove(ops, 0);
-        }
-        if (StringUtils.isBlank(ops[ops.length - 1])) {
-            ArrayUtils.remove(ops, ops.length - 1);
-        }
+        String[] nums = expression.split(SPLIT_FOR_NUMBER);
+        String[] ops = getOperations(expression);
         List<String> postRetrieveList = changeToPostRetrieve(nums, ops);
         return calculate(postRetrieveList);
     }
 
-    private List<String> changeToPostRetrieve(String[] nums, String[] ops) {
-        Map<String, Integer> priorityMap = new HashMap<>(8);
-        priorityMap.put("+", 0);
-        priorityMap.put("-", 0);
-        priorityMap.put("*", 1);
+    private String[] getOperations(String expression) {
+        String[] ops = expression.split(SPLIT_FOR_OPERATION);
+        if (isBlank(ops[0])) {
+            remove(ops, 0);
+        }
+        if (isBlank(ops[ops.length - 1])) {
+            remove(ops, ops.length - 1);
+        }
+        return ops;
+    }
 
+    private List<String> changeToPostRetrieve(String[] nums, String[] ops) {
         Stack<String> myStack = new Stack<>();
         List<String> list = new ArrayList<>(nums.length + ops.length);
-        int point = -1;
         for (int i = 0; i < nums.length; i++) {
             if (ops[i].length() != 0) {
                 while (!myStack.isEmpty() && priorityMap.get(ops[i]) <= priorityMap.get(myStack.peek())) {
@@ -61,11 +73,11 @@ public class Calculator {
             } else {
                 int b = stack.pop();
                 int a = stack.pop();
-                if (obj.equals("+"))
+                if ("+".equals(obj))
                     stack.add(a + b);
-                if (obj.equals("-"))
+                if ("-".equals(obj))
                     stack.add(a - b);
-                if (obj.equals("*"))
+                if ("*".equals(obj))
                     stack.add(a * b);
             }
         }
